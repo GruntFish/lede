88ced782ca3f60ee8b5024e01b40b8e879a93ed1
mclk623 <mclk623@163.com>
2025-10-03 05:50:36 +0000

    add mtk hnat and mt7915d driver

--- target/linux/ramips/mt7621/target.mk
+++ target/linux/ramips/mt7621/target.mk
@@ -10,7 +10,8 @@ KERNELNAME:=vmlinux vmlinuz
 # make Kernel/CopyImage use $LINUX_DIR/vmlinuz
 IMAGES_DIR:=../../..
 
-DEFAULT_PACKAGES += kmod-mtk-hnat wpad-openssl swconfig
+DEFAULT_PACKAGES += wpad-basic-wolfssl kmod-mtk-hnat swconfig \
+			iptables-mod-ipopt iptables-mod-conntrack-extra \
 
 define Target/Description
 	Build firmware images for Ralink MT7621 based boards.
